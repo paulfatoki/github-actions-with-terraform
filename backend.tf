f288b6df27a8a1b4d40a4973c1b75a1077bddb57
clement2019 <ayeniayokunle@yahoo.co.uk>
2024-10-07 22:14:08 +0100

    9

--- backend.tf
+++ backend.tf
@@ -1,7 +1,15 @@
 terraform {
   backend "s3" {
-    bucket = "bucket-eks-cluster-100"
-    key    = "project/terraform.tfstate"
-    region = "eu-west-2"
+    bucket         = "my-terraform-state-clement11"
+    region         = "eu-west-2"
+    key            = "aws-github-actions/terraform.tfstate"
+    encrypt = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
   }
 }
