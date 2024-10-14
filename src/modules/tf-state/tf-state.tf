019712af012d582346a15ec6b0006bda481c080a
clement2019 <ayeniayokunle@yahoo.co.uk>
2024-10-14 15:39:14 +0100

    freshly3

--- src/modules/tf-state/tf-state.tf
+++ src/modules/tf-state/tf-state.tf
@@ -1,21 +1,56 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
 # S3 Bucket for TF State File
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = var.bucket_name
+resource "aws_s3_bucket" "example" {
+  bucket        = "jenkins-bucket-204567"
   force_destroy = true
 }
 
-resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket = aws_s3_bucket.terraform_state.bucket
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  enable_key_rotation     = true
+  deletion_window_in_days = 7
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
