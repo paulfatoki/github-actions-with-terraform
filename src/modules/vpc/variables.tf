406d846416de1d424d3ee13c1739ef50b795bed4
clement2019 <ayeniayokunle@yahoo.co.uk>
2024-10-14 15:15:00 +0100

    freshly1

--- src/modules/vpc/variables.tf
+++ src/modules/vpc/variables.tf
@@ -1,8 +1,4 @@
-variable "vpc_cidr" {
-  description = "CIDR for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
+
 variable "vpc_cidr" {
   description = "CIDR for VPC"
   type        = string
